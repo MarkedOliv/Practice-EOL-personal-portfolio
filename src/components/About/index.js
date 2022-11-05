435b9811059dcf76562fcdd1397a0354b964155d
MarkedOlive <olivdev4@gmail.com>
2022-11-05 16:32:42 -0400

    Fixing typo

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,7 @@ const About = () => {
         <div className='container about-page'>
             <div className='text-zone'>
                 <h1>About Me</h1>
-                <p>Interested in the BackEnd area with the ability to made web projects, always opting for them to have high scalability and modularizing to make it an easier task to work with my team.
+                <p>Interested in the BackEnd area with the ability to make web projects, always opting for them to have high scalability and modularizing to make it an easier task to work with my team.
                 Experience working in JavaScript, NodeJS, React, Redux, SQL, PostgreSQL, Express, MongoDB, JWT, Firebase, etc.</p>
                 <p>I am a tidy, patient, resilient programmer who strives to solve problems.</p>
                 <p>I got interested in the world of programming due to family influences, and I'm so happy fot that since now I have skills that I never thought I would and I will continue on this path, striving to improve every day.</p>
