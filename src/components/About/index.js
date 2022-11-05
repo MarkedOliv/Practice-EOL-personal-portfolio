93e6c5405e4174a68b91304ae9b12f8d51bac529
MarkedOlive <olivdev4@gmail.com>
2022-11-04 20:24:40 -0400

    Done for now to test deploy netlify

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,3 @@
-import './index.scss';
 import Photo from '../Photo';
 
 const About = () => {
--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,10 +1,12 @@
 import './index.scss';
-import logo from '../../assets/img/programacion-web.png'
+import logo from '../../assets/img/cv-preview.PNG';
+import document from '../../assets/CV-Img-EN.pdf';
 
 const Logo = () => {
     return (
         <div className='logo-container'>
-            <img className='solid-logo' src={logo} alt='logo'/>
+            <img className='solid-logo' src={logo} alt='CV-Preview'/>
+            <a href={document} download className='flat-button'>Download CV</a>
         </div>
     )
 }
